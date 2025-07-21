2d68e558afc449dec632f85654448a983f983069
Oleksandr Kostenko <sanyokkua@gmail.com>
2025-07-21 20:53:01 +0200

    Added new prompts (#5)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 'use client';
 import NextDocument, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Link from 'next/link';
 
 export default class Document extends NextDocument {
     static getInitialProps(ctx: DocumentContext) {
@@ -10,13 +11,13 @@ export default class Document extends NextDocument {
         return (
             <Html suppressHydrationWarning>
                 <Head>
-                    <link rel="icon" href="/favicon.ico?" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?" />
-                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?" />
-                    <link rel="android-chrome" sizes="192x192" href="/android-chrome-192x192.png?" />
-                    <link rel="android-chrome" sizes="512x512" href="/android-chrome-512x512.png?" />
-                    <link rel="manifest" href="/site.webmanifest" />
+                    <Link rel="icon" href="/favicon.ico?" />
+                    <Link rel="icon" type="image/png" href="/favicon-16x16.png?" />
+                    <Link rel="icon" type="image/png" href="/favicon-32x32.png?" />
+                    <Link rel="apple-touch-icon" href="/apple-touch-icon.png?" />
+                    <Link rel="android-chrome" href="/android-chrome-192x192.png?" />
+                    <Link rel="android-chrome" href="/android-chrome-512x512.png?" />
+                    <Link rel="manifest" href="/site.webmanifest" />
                 </Head>
                 <body>
                     <Main />
