31226811651ecb83c315915c40c8b08dae62659e
Oleksandr Kostenko <sanyokkua@gmail.com>
2026-06-18 23:42:02 +0200

    test(setup): mock URL.createObjectURL and revokeObjectURL to silence jsdom noise

--- test/setup.ts
+++ test/setup.ts
@@ -4,3 +4,6 @@ import { TextDecoder, TextEncoder } from 'node:util';
 
 Object.assign(globalThis, { TextDecoder, TextEncoder });
 Object.defineProperty(globalThis, 'crypto', { value: webcrypto, writable: false });
+
+global.URL.createObjectURL = jest.fn(() => 'blob:mock-url');
+global.URL.revokeObjectURL = jest.fn();
